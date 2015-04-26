6258f8b5e4aa90535c30368bfdf1faa44c6401b6
dileepvr <dileepvr@gmail.com>
2015-04-25 22:21:23 -0700

    Shoes and shuffles ready on cpu side

---
+++ fileio.h
@@ -0,0 +1,4 @@
+void get_real_param(char*, char*, float*, bool);
+void get_int_param(char*, char*, int*, bool);
+void get_str_param(char*, char*, char*, bool);
+void get_bool_param(char*, char*, bool*, bool);
---
+++ main.h
@@ -0,0 +1,4 @@
+void read_params(char*);
+void swap(int*, int*);
+void shuffle(int*, int);
+void initialize_shoe(int*, int);
